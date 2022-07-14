b62dc57dd2c07a482ea98971a83d1fadf1140acd
Dragan-Stojanovski <100153691+Dragan-Stojanovski@users.noreply.github.com>
2022-07-14 17:23:35 +0200

    fix-1

--- script-blog-2.js
+++ script-blog-2.js
@@ -226,22 +226,3 @@ if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
 
 
 
-
-let slideImg=document.getElementById("slideImg");
-var images=new Array(
-    "https://images.pexels.com/photos/4509131/pexels-photo-4509131.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    "https://images.pexels.com/photos/196645/pexels-photo-196645.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-   
-);
-
-var len = images.length;
-var i = 0;
-
-function slider(){
-if(i>len-1){
-    i=0;
-}
-slideImg.src=images[i];
-i++;
-setTimeout('slider()',4000);
-}
